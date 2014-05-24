b18b93b2d573e55b4a503b27aed2106fa27fab2f
mclxly <mclxly@gmail.com>
2014-05-24 17:01:59 +0800

    update setting for sublime

--- ruby/t1.rb
+++ ruby/t1.rb
@@ -1,14 +1,27 @@
-require 'benchmark/ips'
+# require 'benchmark/ips'
 
-def slow(&block)
-	block
-end
+# def slow(&block)
+# 	block
+# end
 
-def fast
-	yield	
-end
+# def fast
+# 	yield	
+# end
 
-Benchmark.ips do |x|
-	x.report('slow')	{ slow { 1 + 1} }
-	x.report('fast')	{ fast { 1 + 1} }
-end
+# Benchmark.ips do |x|
+# 	x.report('slow')	{ slow { 1 + 1} }
+# 	x.report('fast')	{ fast { 1 + 1} }
+# end
+
+# names = ['xiaoming', 'xiaohong', 'dage']
+
+# tt = names.select{ |name| name.start_with?('x') }.map { |name| name.upcase  }
+
+# p(tt)
+
+#####################
+
+require 'abbrev'
+require 'pp'
+
+p Abbrev.abbrev(['ruby', 'rules'])
