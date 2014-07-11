1bc76d5efdf7d235c815e862dde4f372dd3f51e5
mclxly <mclxly@gmail.com>
2014-07-11 16:59:22 +0800

    update python

--- python/base/t1.py
+++ python/base/t1.py
@@ -1,5 +1,42 @@
-values = [16, 3, 2, 13]
+import sys
+from random import randint
 
-total = sum(values)
+print(sys.version)
 
-print total
+def main() :
+  print("main")
+
+main()
+
+raise SystemExit
+
+CORRECT_ANSWERS = "adbdcacbdac"
+
+done = False
+while not done:
+  userAnswers = input("Enter your exam answers: ")
+  if len(userAnswers) == len(CORRECT_ANSWERS):
+    done = True
+  else:
+    print("Error: an incorrect number")
+
+numQuestions = len(CORRECT_ANSWERS)
+numCorrect = 0
+results = ""
+
+for i in range(numQuestions) :
+  if userAnswers[i] == CORRECT_ANSWERS[i] :
+    numCorrect += 1
+    results += userAnswers[1]
+  else :
+    results += "X"
+
+score = round(numCorrect / numQuestions * 100)
+
+if score == 100 :
+  print("Very Good!")
+else :
+  print("You missed %d questions: %s" % (numQuestions - numCorrect, results))
+
+print("Your score is: %d percent" % score)
+
