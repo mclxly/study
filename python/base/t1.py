ccbd6b68b280c570310e022c1abd6d59773e5493
mclxly <mclxly@gmail.com>
2014-07-12 17:02:44 +0800

    update python

--- python/base/t1.py
+++ python/base/t1.py
@@ -3,40 +3,58 @@ from random import randint
 
 print(sys.version)
 
-def main() :
-  print("main")
+#################################################
+def readFloats(numberOfInputs):
+  print("Enter", numberOfInputs, "numbers:")
+  inputs = []
+  for i in range(numberOfInputs):
+    value = float(input(''))
+    inputs.append(value)
+
+  return inputs
+
+def main():
+  numbers = readFloats(5)
+  print(numbers)
 
 main()
 
 raise SystemExit
 
-CORRECT_ANSWERS = "adbdcacbdac"
 
-done = False
-while not done:
-  userAnswers = input("Enter your exam answers: ")
-  if len(userAnswers) == len(CORRECT_ANSWERS):
-    done = True
-  else:
-    print("Error: an incorrect number")
+# def main():
+#     print("main")
 
-numQuestions = len(CORRECT_ANSWERS)
-numCorrect = 0
-results = ""
+# main()
 
-for i in range(numQuestions) :
-  if userAnswers[i] == CORRECT_ANSWERS[i] :
-    numCorrect += 1
-    results += userAnswers[1]
-  else :
-    results += "X"
+# raise SystemExit
 
-score = round(numCorrect / numQuestions * 100)
+# CORRECT_ANSWERS = "adbdcacbdac"
 
-if score == 100 :
-  print("Very Good!")
-else :
-  print("You missed %d questions: %s" % (numQuestions - numCorrect, results))
+# done = False
+# while not done:
+#     userAnswers = input("Enter your exam answers: ")
+#     if len(userAnswers) == len(CORRECT_ANSWERS):
+#         done = True
+#     else:
+#         print("Error: an incorrect number")
 
-print("Your score is: %d percent" % score)
+# numQuestions = len(CORRECT_ANSWERS)
+# numCorrect = 0
+# results = ""
 
+# for i in range(numQuestions):
+#     if userAnswers[i] == CORRECT_ANSWERS[i]:
+#         numCorrect += 1
+#         results += userAnswers[1]
+#     else:
+#         results += "X"
+
+# score = round(numCorrect / numQuestions * 100)
+
+# if score == 100:
+#     print("Very Good!")
+# else:
+#     print("You missed %d questions: %s" % (numQuestions - numCorrect, results))
+
+# print("Your score is: %d percent" % score)
